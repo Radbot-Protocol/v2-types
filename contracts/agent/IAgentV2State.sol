8b3b3185943968d1b9273d7b8552d330902ccb47
radbot <211224137+bxkdhc6282@users.noreply.github.com>
2025-10-10 01:02:34 -0700

    chore-1

--- contracts/agent/IAgentV2State.sol
+++ contracts/agent/IAgentV2State.sol
@@ -6,5 +6,8 @@ import {IAgentV2Types} from "./IAgentV2Types.sol";
 interface IAgentV2State {
     function getAttributes(
         uint256 tokenId
-    ) external view returns (IAgentV2Types.AgentV2Attributes memory);
+    )
+        external
+        view
+        returns (uint256 deployments, uint256 yield, bool isDeployed);
 }
