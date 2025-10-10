6a45a04e818095b4a3ac104bdeeed77fbb8b45e1
radbot <211224137+bxkdhc6282@users.noreply.github.com>
2025-10-10 02:12:30 -0700

    chore-4

--- contracts/agent/IAgentV2State.sol
+++ contracts/agent/IAgentV2State.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.20;
 
+import {IAgentV2Types} from "./IAgentV2Types.sol";
+
 interface IAgentV2State {
     function getAttributes(
-        uint256 deployments,
-        uint256 yield,
-        bool isDeployed
-    ) external view returns (uint256);
+        uint256 tokenId
+    ) external view returns (IAgentV2Types.AgentV2Attributes memory);
 }
