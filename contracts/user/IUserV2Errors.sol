49e561bd6b1cb0d9dcdd330b479674dd19059321
oxdefiprof <85069769+ibold-dev@users.noreply.github.com>
2025-10-10 22:34:03 -0700

    user_2

--- contracts/user/IUserV2Errors.sol
+++ contracts/user/IUserV2Errors.sol
@@ -5,6 +5,7 @@ interface IUserV2Errors {
     error UserV2Errors__UsernameAlreadyTaken(bytes16 username);
     error UserV2Errors__UsernameInvalid(bytes16 username);
     error UserV2Errors__UserDoesNotExist(address user);
+    error UserV2Errors__UserAlreadyExists(address user);
     error UserV2Errors__PageSizeTooLarge(uint256 pageSize);
     error UserV2Errors__ZeroAddress();
 }
